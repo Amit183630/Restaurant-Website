184b859864a45050f36e190eff071dbfd58baf6f
Amit Kumar Gupta <Amit183630@gmail.com>
2020-05-21 19:40:08 +0530

    Menu Category

--- src/common/menu.service.js
+++ src/common/menu.service.js
@@ -5,9 +5,18 @@
   MenuService.$inject=['$http','ApiPath']
   function MenuService($http,ApiPath) {
     var serice=this;
-    this.getCategories=function () {
+    service.getCategories=function () {
       return $http.get(ApiPath+"/categories.json").then(function (response) {
         return response.data;
+      });
+    }
+    service.getMenuItems=function (category) {
+      var config={};
+      if(category){
+        config.params={ 'category':category }
+      }
+      return $http.get(ApiPath+'/menu_items.json',config).then(function (response) {
+        return reponse.data;
       });
     }
   }
